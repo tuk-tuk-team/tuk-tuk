d8c3c64e4f55bf48c247e1034a96ca2f7b9f2803
danil0110 <danilmail0110@gmail.com>
2021-03-06 17:47:54 +0200

    Webpack build works

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+  // watch: true,
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
