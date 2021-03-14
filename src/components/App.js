2d44857c04812409d082e53299812e10f1fd346f
danil0110 <danilmail0110@gmail.com>
2021-03-14 14:04:39 +0200

    Fixed eslint & prettier scripts

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,10 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<h1>My React App!</h1>
+				<h1> My React App!</h1>
 			</div>
 		);
 	}
 }
-let a, b, c;
-var b,
-c;
 
 export default App;
