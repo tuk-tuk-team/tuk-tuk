0ccc8ce6342e0630454eff6ebf18a66800273f3b
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-15 12:42:12 +0300

    Added logic for adding new post

--- client/services/index.js
+++ client/services/index.js
@@ -9,11 +9,28 @@ export default class Service {
         return await response.json();
     }
 
+    async postData(url, body) {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body)
+        });
+
+        if (!response.ok) {
+            throw new Error(`Couldn't post ${url}. Status: ${response.status} - ${response.statusText}`)
+        }
+
+        return await response.json();
+    }
+
     async getAllPosts() {
         return await this.getData('/api/posts');
     }
 
     async getPostById(id) {
         return await this.getData(`/api/posts/${id}`);
+    }
+
+    async createPost(body) {
+        return await this.postData('/api/posts/add', body);
     }
 }
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+
 async function routes(fastify, options) {
     fastify.get('/', async (request, reply) => {
         try {
@@ -23,6 +25,32 @@ async function routes(fastify, options) {
             } else {
                 reply.send({ error: 'Post not found' });
             }
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    fastify.post('/add', async (request, reply) => {
+        try {
+            const postId = uuidv4();
+            const {
+                type,
+                title,
+                description,
+                district,
+                location,
+                ownerPhone,
+                price,
+                originLink
+            } = JSON.parse(request.body);
+
+            const res = await fastify.db.query(`
+                INSERT INTO posts ("postId", "type", "title", "description", "originLink", "district", "location", "ownerPhone", "price")
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+                RETURNING *
+            `, [postId, type, title, description, originLink, district, location, ownerPhone, price]);
+
+            reply.send(res.rows[0]);
         } catch (e) {
             console.log(e);
         }
