e7c4dffe5d77b76b1d3c6020c3099874a3081f00
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-28 16:26:58 +0300

    Refactored service

--- client/services/index.js
+++ client/services/index.js
@@ -1,53 +1,65 @@
+const getData = async (url) => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Couldn't fetch ${url}. Status: ${response.status} - ${response.statusText}`);
+    }
+
+    return await response.json();
+}
+
+const postData = async (url, body) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+        throw new Error(`Couldn't post ${url}. Status: ${response.status} - ${response.statusText}`);
+    }
+
+    return await response.json();
+}
+
+const editData = async (url, body) => {
+    const response = await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+        throw new Error(`Couldn't edit ${url}. Status: ${response.status} - ${response.statusText}`);
+    }
+
+    return await response.json();
+}
+
+const deleteData = async (url) => {
+    const response = await fetch(url, {
+        method: 'DELETE'
+    });
+
+    if (!response.ok) {
+        throw new Error(`Couldn't delete ${url}. Status: ${response.status} - ${response.statusText}`);
+    }
+
+    return await response.json();
+}
+
 export default class Service {
-    async getData(url) {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error(`Couldn't fetch ${url}. Status: ${response.status} - ${response.statusText}`);
-        }
-
-        return await response.json();
-    }
-
-    async postData(url, body) {
-        const response = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(body)
-        });
-
-        if (!response.ok) {
-            throw new Error(`Couldn't post ${url}. Status: ${response.status} - ${response.statusText}`)
-        }
-
-        return await response.json();
-    }
-
-    async editData(url, body) {
-        const response = await fetch(url, {
-            method: 'PUT',
-            body: JSON.stringify(body)
-        });
-
-        if (!response.ok) {
-            throw new Error(`Couldn't edit ${url}. Status: ${response.status} - ${response.statusText}`)
-        }
-
-        return await response.json();
-    }
-
     async getAllPosts() {
-        return await this.getData('/api/posts');
+        return await getData('/api/posts');
     }
 
     async getPostById(id) {
-        return await this.getData(`/api/posts/${id}`);
+        return await getData(`/api/posts/${id}`);
     }
 
     async createPost(body) {
-        return await this.postData('/api/posts/add', body);
+        return await postData('/api/posts/add', body);
     }
 
     async editPost(id, body) {
-        return await this.editData(`/api/posts/${id}/edit`, body);
+        return await editData(`/api/posts/${id}/edit`, body);
     }
 }
