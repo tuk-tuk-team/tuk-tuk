4597d2612bee28d0bf6ae2117ee381eee77bdd81
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-05-06 11:06:39 +0300

    Removed try-catch in routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -6,60 +6,30 @@ async function routes(fastify, options) {
     const queriesData = queries(fastify.db)
 
     fastify.get('/', async (request, reply) => {
-        try {
-
-            const result = await queriesData.getAllPostsData();
-            reply.send(result.rows)
-
-        } catch (e) {
-            console.log(e);
-        }
+        const result = await queriesData.getAllPostsData();
+        reply.send(result.rows);
     });
 
     fastify.get('/:id', async (request, reply) => {
-        try {
-
-            const result = await queriesData.getPostByIdData(request.params.id);
-            reply.send(result.rows[0]);
-
-        } catch (e) {
-            console.log(e);
-        }
+        const result = await queriesData.getPostByIdData(request.params.id);
+        reply.send(result.rows[0]);
     });
 
     fastify.post('/add', async (request, reply) => {
-        try {
-
-            const postId = uuidv4();
-            const result = await queriesData.addPostData(postId, request.body);
-            reply.send(result.rows[0]);
-
-        } catch (e) {
-            console.log(e);
-        }
+        const postId = uuidv4();
+        const result = await queriesData.addPostData(postId, request.body);
+        reply.send(result.rows[0]);
     });
 
     fastify.put('/:id/edit', async (request, reply) => {
-        try {
-
-            const result = await queriesData.editPostData(request.body);
-            reply.send(result.rows[0]);
-
-        } catch (e) {
-            console.log(e);
-        }
+        const result = await queriesData.editPostData(request.body);
+        reply.send(result.rows[0]);
     });
 
     fastify.delete('/:id/delete', async (request, reply) => {
-        try {
-
-            const postId = request.params.id;
-            const result = await queriesData.deletePostData(postId);
-            reply.send(result.rows[0]);
-            
-        } catch (e) {
-            console.log(e);
-        }
+        const postId = request.params.id;
+        const result = await queriesData.deletePostData(postId);
+        reply.send(result.rows[0]);
     });
 }
 
