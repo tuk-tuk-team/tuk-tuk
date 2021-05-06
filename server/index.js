2f2e5acffb58b0ad1bd0b576d8b09c772df6e569
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-05-06 21:49:57 +0300

    Cleaned up the root file and registered plugins

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ const dbConn = require('./plugins/db');
 const postRoutes = require('./routes/postRoutes');
 
 fastify.register(dbConn);
-fastify.register(postRoutes, { prefix: '/api/posts' });
-
-fastify.get('/', (request, reply) => {
-    reply.send({ hello: 'world!' });
+fastify.register(require('fastify-cookie'));
+fastify.register(require('fastify-cors'), {
+	credentials: true,
+	origin: ['http://localhost:3000']
 });
+
+fastify.register(postRoutes, { prefix: '/api/posts' });
 
 const PORT = process.env.PORT || 4000;
 
 fastify.listen(PORT, (err, address) => {
-    console.log(`Server is running on ${address}`);
+	console.log(`Server is running on ${address}`);
 });
