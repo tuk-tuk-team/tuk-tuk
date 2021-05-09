47e8c415403e1b672d015a80c1ee549ffc549bb1
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-05-09 19:39:37 +0300

    Added login function

--- client/components/app/app.js
+++ client/components/app/app.js
@@ -4,26 +4,65 @@ import { Container } from 'reactstrap';
 import './app.css';
 
 import Header from '../header';
-import { FeedPage, ProfilePage, PostPage, NewPostPage, Login, SignUpMenu, EditProfile, EditPost} from '../pages';
+import {
+	FeedPage,
+	ProfilePage,
+	PostPage,
+	NewPostPage,
+	Login,
+	SignUpMenu,
+	EditProfile,
+	EditPost
+} from '../pages';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			authUser: null
+		};
+
+		this.loginUser = this.loginUser.bind(this);
+		this.logoutUser = this.logoutUser.bind(this);
+	}
+
+	loginUser(user) {
+		this.setState({
+			authUser: { ...user }
+		});
+	}
+
+	logoutUser() {
+		this.setState({ authUser: null });
+	}
+
 	render() {
 		return (
 			<>
 				<Router>
-					<Header />
+					<Header authUser={this.state.authUser} />
 					<Container>
-                        <Switch>
-                            <Route exact path="/" render={() => <h1 className="mt-5">Welcome to Tuk-Tuk</h1>} />
-                            <Route exact path="/login" component={Login} />
-                            <Route exact path="/posts" component={FeedPage} />
-                            <Route exact path="/posts/add" component={NewPostPage} />
-                            <Route exact path="/posts/:id" component={PostPage} />
-                            <Route exact path="/profile" component={ProfilePage} />
+						<Switch>
+							<Route
+								exact
+								path="/"
+								render={() => <h1 className="mt-5">Welcome to Tuk-Tuk</h1>}
+							/>
+							<Route
+								exact
+								path="/login"
+								component={(props) => (
+									<Login loginUser={this.loginUser} {...props} />
+								)}
+							/>
+							<Route exact path="/posts" component={FeedPage} />
+							<Route exact path="/posts/add" component={NewPostPage} />
+							<Route exact path="/posts/:id" component={PostPage} />
+							<Route exact path="/profile" component={ProfilePage} />
 							<Route exact path="/register" component={SignUpMenu} />
 							<Route exact path="/editProfile" component={EditProfile} />
 							<Route exact path="/posts/:id/edit" component={EditPost} />
-                        </Switch>
+						</Switch>
 					</Container>
 				</Router>
 			</>
--- client/components/pages/login.js
+++ client/components/pages/login.js
@@ -1,57 +1,81 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AuthService from '../../services/auth-service';
 import './login.css';
 
 export default class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			login: '',
-			password: ''
+			data: {
+				email: '',
+				password: ''
+			},
+			error: null
 		};
+		this.authService = new AuthService();
 
 		this.handlerInput = this.handlerInput.bind(this);
-		this.login = this.login.bind(this);
+		this.onLogin = this.onLogin.bind(this);
 	}
 
-	handlerInput(event) {
-		event.preventDefault();
-		const target = event.target;
-		const value = target.value;
-		const name = target.name;
-		this.setState({
-			[name]: value
+	handlerInput(e) {
+		const { name, value } = e.target;
+		this.setState(({ data }) => {
+			data[name] = value;
 		});
 	}
 
-	login() {
-		console.log(this.state);
+	onLogin(e) {
+		e.preventDefault();
+		this.authService.login(this.state.data).then((data) => {
+			if (!data.error) {
+				this.props.loginUser(data);
+				return this.props.history.push('/');
+			}
+
+			this.setState({
+				error: data.message
+			});
+		});
 	}
 
 	render() {
+		if (this.state.error) {
+			console.log(this.state.error);
+		}
+
 		return (
 			<div className="container-login-page">
 				<h2>Увійти до облікового запису</h2>
 				<div className="login-inputs">
-					<input
-						placeholder="Введіть номер телефону або email"
-						className="login"
-						name="login"
-						type="text"
-						value={this.state.login}
-						onChange={this.handlerInput}
-					></input>
-					<input
-						placeholder="Введіть пароль"
-						className="password"
-						name="password"
-						type="password"
-						value={this.state.password}
-						onChange={this.handlerInput}
-					></input>
-					<button className="btn btn-danger" type="reset" onClick={this.login}>
-						Увійти
-					</button>
+					<form method="POST">
+						<input
+							placeholder="Введіть email"
+							className="login"
+							name="email"
+							type="email"
+							value={this.state.email}
+							onChange={this.handlerInput}
+							required
+						></input>
+						<input
+							placeholder="Введіть пароль"
+							className="password"
+							name="password"
+							type="password"
+							value={this.state.password}
+							onChange={this.handlerInput}
+							required
+						></input>
+						<button
+							className="btn btn-danger"
+							type="submit"
+							onClick={this.onLogin}
+						>
+							Увійти
+						</button>
+					</form>
 					<Link to="/restore_password">Забули пароль?</Link>
 					<div className="no-acc">
 						<span>Немає аккаунта? </span>
