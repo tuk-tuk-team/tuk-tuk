0650d066277b5e564dc4a557c21e92fe80636d4c
Dariia Kariavka <koalaansthen@gmail.com>
2021-05-23 23:23:08 +0300

    unit tests add-on for adding post and new-post-page

--- client/__tests__/add-post-form.test.js
+++ client/__tests__/add-post-form.test.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import AddPostForm from '../components/add-post-form/add-post-form'
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import AddPostForm from '../components/add-post-form/add-post-form';
+import PostService from '../services/post-service'
+
 
 Enzyme.configure({ adapter: new Adapter() });
+jest.mock('../services/post-service');
 
 describe('AddPostFormTest', ()=>{
 
-    it('renders without crashing', () => {
+    it('renders correctly', () => {
         shallow(<AddPostForm/>)
     });
 
-    it('test default props of class', () => {
-        const wrapper = shallow(<AddPostForm />);
-        expect(wrapper.state('type')).toBe(1);
-      })
+    describe('Functions tests', ()=>{
 
-    it('test onInputChange method', () => {
+        it('test onInputChange function', () => {
+            const component = shallow(<AddPostForm />);
+            
+            component.find('input[name="type"]').at(0).simulate('change', { target: { name: 'type', value: 1 }});
+            expect(component.state('type')).toBe(1);
+    
+            component.find('input[name="title"]').simulate('change', { target: { name: 'title', value: 'CityName' }});
+            expect(component.state('title')).toBe("CityName");
+
+            component.find('input[name="address"]').simulate('change', { target: { name: 'address', value: 'Adress' }});
+            expect(component.state('address')).toBe("Adress");
+
+            component.find('input[name="price"]').simulate('change', { target: { name: 'price', value: 25 }});
+            expect(component.state('price')).toBe(25);
+        });
+
+        
+
+    });
+
+
+    it('Check onChange with state.type === 1', () => {
         const onInputChangeMock = jest.fn();
 
         AddPostForm.prototype.onInputChange = onInputChangeMock;
@@ -28,7 +49,21 @@ describe('AddPostFormTest', ()=>{
         expect(onInputChangeMock).toHaveBeenCalled();
     });
 
-    it("test onSubmit method", ()=>{
+    it('Check onChange with state.type === 2', () => {
+        const onInputChangeMock = jest.fn();
+
+        AddPostForm.prototype.onInputChange = onInputChangeMock;
+
+        let component = shallow(<AddPostForm />);
+
+        component.find('input').first().simulate('change', { target: { value: 'custom' }});
+        component.setState({type: null});
+        component.update();
+
+        expect(onInputChangeMock).toHaveBeenCalled();
+    });
+
+    it("Check onSubmit button", ()=>{
         const onSubmitMock = jest.fn();
 
         AddPostForm.prototype.onSubmit = onSubmitMock;
@@ -39,14 +74,4 @@ describe('AddPostFormTest', ()=>{
         expect(onSubmitMock).toHaveBeenCalled();
     });
 
-   
 });
-
-
-    
-
-
-
-
-
-
